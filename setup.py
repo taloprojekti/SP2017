0ea7ef934e378800007569546388a6ab5a5207bb
make <markus.saynevirta@gmail.com>
2017-03-07 17:44:04 +0200

    luodaan setup.py määritystiedosto

---
+++ setup.py
@@ -0,0 +1,10 @@
+# Setup-tiedosto
+
+def Tmin():
+	return int(19) # Minimilämpötila, johon rakennuksen sisälämmön annetaan laskea.
+
+def Tmax():
+	return int(23) # Maksimilämpötila, johon rakennuksen sisälämmön annetaan nousta.
+
+def Tfav():
+	return int(21) # Suosikkilämpötila, johon rakennuksen sisälämpö asetetaan, kun päivän sähkönhinnat ovat tasaiset.
