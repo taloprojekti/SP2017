b5aaf1ee20223861ef2f1ac5fd5457fa973c7d5e
make <markus.saynevirta@gmail.com>
2017-03-14 19:53:45 +0200

    rele-ohjelman kehitystä

--- rele.py
+++ rele.py
@@ -1,12 +1,25 @@
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 
-while True:
-	comm = input("syota komento\n")
-	if comm != "stop":
-		GPIO.output(rele_pin, GPIO.HIGH)
-	else:
-		print("keskeytetty")
-		break
-
-GPIO.output(rele_pin, GPIO.LOW)
+def virta_on(rele_pin):
+	GPIO.setup(rele_pin, GPIO.OUT)
+	GPIO.output(rele_pin, GPIO.HIGH)
+	
+def virta_off(rele_pin):
+	GPIO.output(rele_pin, GPIO.LOW)
+	
+def cleanup():
+	GPIO.cleanaup()
+	
+def switch(mode, PID, temp_req, temp_now, deadband_max, deadband_min, rele_pin)
+	if mode == "active":
+		if temp_req > temp_now:
+			virta_on(rele_pin)
+		else:
+			virta_off(rele_pin)
+			
+	elif mode == "PIDctrl":
+		if PID > deadband_max:
+			virta_on(rele_pin)
+		if PID < deadband:
+			virta_off(rele_pin)
