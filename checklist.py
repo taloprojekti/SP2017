0e28abd01ec6e69ab7ac9046340a9f8608672bf6
oliverjarnefelt <oliver.jarnefelt@gmail.com>
2017-04-07 16:09:13 +0300

    Update checklist.py

--- checklist.py
+++ checklist.py
@@ -4,7 +4,15 @@ def main():
 	p = nownew.day
 	k = nownew.month
 	v = nownew.year
-	strN = str(p) + str(k) + str(v)
+	if p < 10: 
+		if k < 10:
+			strN = "0" + str(p) + "0" + str(k) + str(v)
+		else:	
+			strN = "0" + str(p) + str(k) + str(v)
+	if k < 10 and p > 10:
+		strN = str(p) + "0" + str(k) + str(v)
+	else:	
+		strN = str(p) + str(k) + str(v)
 	file = open("checklist.txt", "r")
 	strL = file.read()
 	if  strN != strL:
