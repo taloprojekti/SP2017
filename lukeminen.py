ffa58c9b2f83f6aa30b2f30e5b42d9e68dddb967
Markus Saynevirta <markus.saynevirta@gmail.com>
2017-06-04 23:16:02 +0300

    legacy-koodin karsintaa

--- lukeminen.py
+++ lukeminen.py
@@ -1,28 +1,13 @@
 # Tulkintaohjelma Nordpoolin day ahead -hintatietojen lukemiseen
 # txt-tiedostosta ja niiden tulkintaan aikatauluttaja-ohjelmaa varten
 
-######### DATAN TUOTTO #########
-
-jarj=[] #Alustetaan hintajärjestyksen muuttuja
 minim=0 #Alustetaan minimiarvon muuttuja
-
-def jarjestys(tiedot): # Funktio luo listan tunneista, joka on järjestetty halvimmasta kalleimpaan.
-	i=0
-	keys=tiedot.keys()
-	
-	lista=sorted(keys, key=tiedot.__getitem__)
-	return lista
 
 def minimi(tiedot):
 	print(tiedot)
 	minim = min(tiedot, key=tiedot.get)
 	print("Min: {:.2f}".format(minim))
 	return minim
-	
-def maksimi(tiedot):
-	maxim = max(tiedot, key=tiedot.get)
-	print(maxim)
-	return maxim
 
 def min_delta(tiedot, min): # Funktio laskee tuntien välisen hintaeron. Päivän ensimmäinen tunti näytetään nollana.
 	lista=[]
@@ -34,31 +19,12 @@ def min_delta(tiedot, min): # Funktio laskee tuntien välisen hintaeron. Päivä
 		
 	print(lista)
 	return lista
-		
-def hinta_delta(tiedot): # Funktio laskee tuntien välisen hintaeron. Päivän ensimmäinen tunti näytetään nollana.
-	lista=[]
-	for rivi in tiedot:
-		arvo=float(tiedot[rivi+1]-tiedot[rivi])
-		lista.append(arvo)
-		print("{:.2f}".format(arvo))
-	return lista
-
-def deltadeltaP(delta):
-	i=0
-	ddP=[]
-	while i <= 22:
-		arvo=delta[i+1]-delta[i]
-		ddP.append(arvo)
-		print(arvo)
-		i+=1
-	return ddP
 	
 def sdvparsinta(tiedot, pvm): # Funktio hakee parsinta.py -moduulin, joka lukee hintatiedot sdv-tiedostosta python-sanakirjaksi muuttujaan tiedot
 	import parsinta
 	tiedot = parsinta.luesdv(tiedot, pvm)
 	return tiedot
-	
-######### MAIN #########
+
 def luetiedot(pvm):
 	tiedot={}
 	try:
@@ -69,9 +35,6 @@ def luetiedot(pvm):
 
 		for rivi in tiedot:
 			print(rivi, tiedot[rivi])
-
-		jarj=jarjestys(tiedot)
-		minim=minimi(tiedot)
 
 	except OSError:
 		print("Virhe tiedoston lukemisessa.")
