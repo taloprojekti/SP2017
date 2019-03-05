8e7147d82ddc5be239cff177c07cddeb246d0abd
Markus Säynevirta <25539916+msaynevirta@users.noreply.github.com>
2019-03-05 20:29:16 +0200

    Switching to spaces as an indentation method

--- PIDclass.py
+++ PIDclass.py
@@ -1,32 +1,32 @@
 # Diskreetti PID-säädin
 
 class PID:
-	def __init__(self, Kp, Ki, Kd, Imax, Imin):
-		self.Kp = Kp
-		self.Ki = Ki
-		self.Kd = Kd
-		self.Imax = Imax
-		self.Imin = Imin
-		self.last_i = 0
-		self.last_d = 0
-	
-	def process(self, set_T, curr):
-		err = float(set_T - curr)
-		
-		valP = self.Kp * err # P-termi (proportional)
-		
-		i = float(self.last_i + err) # I-termi (integral)
-		self.last_i = i
-		
-		if i >= self.Imax: # Estetään I-termin rajaton kasvu ja pienentyminen
-			i = self.Imax
-		if i <= self.Imin:
-			i = self.Imin
-		valI = self.Ki*i
-		
-		d = err - self.last_d # D-termi (derivative)
-		self.last_d = d
-		valD = self.Kd*d
-		
-		PID = valP + valI + valD
-		return PID
+    def __init__(self, Kp, Ki, Kd, Imax, Imin):
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+        self.Imax = Imax
+        self.Imin = Imin
+        self.last_i = 0
+        self.last_d = 0
+    
+    def process(self, set_T, curr):
+        err = float(set_T - curr)
+        
+        valP = self.Kp * err # P-termi (proportional)
+        
+        i = float(self.last_i + err) # I-termi (integral)
+        self.last_i = i
+        
+        if i >= self.Imax: # Estetään I-termin rajaton kasvu ja pienentyminen
+            i = self.Imax
+        if i <= self.Imin:
+            i = self.Imin
+        valI = self.Ki*i
+        
+        d = err - self.last_d # D-termi (derivative)
+        self.last_d = d
+        valD = self.Kd*d
+        
+        PID = valP + valI + valD
+        return PID
--- tulkinta.py
+++ tulkinta.py
@@ -1,58 +1,58 @@
 def sisalampotila():
-	sisanyt = tempread.read_temp_in()
-	return sisanyt
-	
+    sisanyt = tempread.read_temp_in()
+    return sisanyt
+    
 def main(day, month, year, hour):
-	# Setup-osa
-	import lukeminen
+    # Setup-osa
+    import lukeminen
 
-	#Muodostetaan str-tyyppinen päiväys muodossa "dd.mm.yy" Nordpoolin hintatietojen lukemiseksi
-	if day < 10:
-		day = str(day)
-		day = str("0"+day)
-	if month < 10:
-		month = str(month)
-		month = str("0"+month)
+    #Muodostetaan str-tyyppinen päiväys muodossa "dd.mm.yy" Nordpoolin hintatietojen lukemiseksi
+    if day < 10:
+        day = str(day)
+        day = str("0"+day)
+    if month < 10:
+        month = str(month)
+        month = str("0"+month)
 
-	pvm = str("{}.{}.{}".format(day, month, year))
-	print(pvm)
+    pvm = str("{}.{}.{}".format(day, month, year))
+    print(pvm)
 
-	tiedot = lukeminen.luetiedot(pvm)
-	minimi = lukeminen.minimi(tiedot)
-	min_delta = lukeminen.min_delta(tiedot, minimi)
-	
-	# Etsitään ohjelma-tilan alut ja loput
+    tiedot = lukeminen.luetiedot(pvm)
+    minimi = lukeminen.minimi(tiedot)
+    min_delta = lukeminen.min_delta(tiedot, minimi)
+    
+    # Etsitään ohjelma-tilan alut ja loput
 
-	print("emptying csv-file\n")
-	open("tasklists/tasklist-prog.csv", "w").close()
+    print("emptying csv-file\n")
+    open("tasklists/tasklist-prog.csv", "w").close()
 
-	print("entering loop")
-	h=0
-	while(True):
-		print("h=", h)
-		for rivi in min_delta[h:]:
-			print(rivi)
-			h+=1
-			print("h=", h)
-			if rivi > 0.19:
+    print("entering loop")
+    h=0
+    while(True):
+        print("h=", h)
+        for rivi in min_delta[h:]:
+            print(rivi)
+            h+=1
+            print("h=", h)
+            if rivi > 0.19:
 
-				f = open("tasklists/tasklist-prog.csv", "w") # Tähän alkuhetken tallennus
-				f.write("start,{:d}-{:d}-{:d},{:d}:{:d}:{:d}\n".format(int(year), int(month), int(day), h-1, 0, 0))
+                f = open("tasklists/tasklist-prog.csv", "w") # Tähän alkuhetken tallennus
+                f.write("start,{:d}-{:d}-{:d},{:d}:{:d}:{:d}\n".format(int(year), int(month), int(day), h-1, 0, 0))
 
-				for rivi in min_delta[h-1:]:
-					print(rivi)
-					h+=1
-					if rivi < 0.19:
-						f = open("tasklists/tasklist-prog.csv", "a") # Tähän päättymishetken tallennus
-						f.write("end,{:d}-{:d}-{:d},{:d}:{:d}:{:d}\n".format(int(year), int(month), int(day), h-1, 0, 0))
-						print("stop") # Tähän päättymishetken tallennus
-						break
-				break
-			
-		# ongelmatilanne, jossa päivän viimeinen luku ei ole alle Pd tai Pdd, jolloin looppi jää avoimeksi.
-		
-		if h==24:
-			break
+                for rivi in min_delta[h-1:]:
+                    print(rivi)
+                    h+=1
+                    if rivi < 0.19:
+                        f = open("tasklists/tasklist-prog.csv", "a") # Tähän päättymishetken tallennus
+                        f.write("end,{:d}-{:d}-{:d},{:d}:{:d}:{:d}\n".format(int(year), int(month), int(day), h-1, 0, 0))
+                        print("stop") # Tähän päättymishetken tallennus
+                        break
+                break
+            
+        # ongelmatilanne, jossa päivän viimeinen luku ei ole alle Pd tai Pdd, jolloin looppi jää avoimeksi.
+        
+        if h==24:
+            break
 
 #Ratkaistaan prog-tilan aikaväli, jonka jälkeen lasketaan mihin lämpötilaan rakennus pitää lämmittää ennen välin alkua.
 #Välin aikana lasketaan milloin pitää lämmittää lisää jotta lämpötila pysyy Tmin yläpuolella.
