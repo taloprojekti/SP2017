fc98de8cdf02aec0d34eb97872c8b91a0c64e489
Tommi Kemmo <46447972+Tommikemmo@users.noreply.github.com>
2019-04-19 22:23:25 +0300

    Cleaned up legacy code, switched to using json datafiles

--- checklist.py
+++ checklist.py
@@ -1,20 +1,13 @@
 def main():    
     from datetime import datetime
+    from jsonhandler import importJSON
     nownew = datetime.now()
     p = nownew.day
     k = nownew.month
     v = nownew.year
-    if p < 10:
-        if k < 10:
-            strN = "0" + str(p) + "0" + str(k) + str(v)
-        else:    
-            strN = "0" + str(p) + str(k) + str(v)
-    if k < 10 and p > 10:
-        strN = str(p) + "0" + str(k) + str(v)
-    else:    
-        strN = str(p) + str(k) + str(v)
-    file = open("tasklists/tasklist-downloader.txt", "r")
-    strL = file.read()
+    strN = str("{:02d}{:02d}{:4d}".format(p, k, v))
+    data = importJSON("tasklists/tasklist.json")
+    strL = data["downloader_time"]
     print(strL, strN)
     if  strN != strL:
         print("Downloader ajo")
