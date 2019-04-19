67235f266accf9c9fe2687e5904ec44791491341
Markus Säynevirta <25539916+msaynevirta@users.noreply.github.com>
2019-04-19 20:00:08 +0300

    Function for writing json files

--- jsonhandler.py
+++ jsonhandler.py
@@ -1,5 +1,6 @@
-from json import load, JSONDecoder, JSONDecodeError
+
 def importJSON(filedir):
+    from json import load, JSONDecoder, JSONDecodeError
     try:
         data = load(open(filedir,'r', encoding='utf-8'))
     
@@ -8,3 +9,11 @@ def importJSON(filedir):
     except JSONDecodeError:
         print('\033[31m' + "Error: " + '\033[0m' + "Failed to read the data file")
     return False
+
+def writeJSON(filedir, data):
+    from json import dumps, JSONEncoder
+    json_data = dumps(data)
+    file = open(filedir, 'w', encoding='utf-8')
+    file.write(json_data)
+
+    return 1
