61e1406cf8a835b8b7f3cae95f577fa7eaed10af
Markus Säynevirta <25539916+msaynevirta@users.noreply.github.com>
2019-04-29 23:36:26 +0300

    Implement context managers

--- jsonhandler.py
+++ jsonhandler.py
@@ -2,19 +2,20 @@
 def importJSON(filedir):
     from json import load, JSONDecoder, JSONDecodeError
     try:
-        data = load(open(filedir,'r', encoding='utf-8'))
-    
+        with open(filedir,'r', encoding='utf-8') as file:
+            data = load(file) 
         return data
 
     except JSONDecodeError:
         print('\033[31m' + "Error: " + '\033[0m' + "Failed to read the data file")
+
     return False
 
 def writeJSON(filedir, data):
-  
     from json import dumps, JSONEncoder
     json_data = dumps(data)
-    file = open(filedir, 'w', encoding='utf-8')
-    file.write(json_data)
+    with open(filedir, 'w', encoding='utf-8') as file:
+        file.write(json_data)
+
     return 1
 
